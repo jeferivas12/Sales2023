801b7f95edb7d0e67135a990cb43bb9b749e207e
jefeRivas <95534532+jefeRivas@users.noreply.github.com>
2023-04-01 12:20:39 -0500

    configuration of web Authentication

---
+++ Sales.WEB/Auth/AuthenticationProviderTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace Sales.WEB.Auth
+{
+    public class AuthenticationProviderTest : AuthenticationStateProvider
+    {
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            await Task.Delay(3000);
+            var anonimous = new ClaimsIdentity();
+            var jeffUser = new ClaimsIdentity(new List<Claim>
+                {
+                    new Claim("FirstName", "Jeferson"),
+                    new Claim("LastName", "Rivas"),
+                    new Claim(ClaimTypes.Name, "jefer@yopmail.com"),
+                    new Claim(ClaimTypes.Role, "Admin")
+                },
+                authenticationType: "test");
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(anonimous)));
+        }
+    }
+}
